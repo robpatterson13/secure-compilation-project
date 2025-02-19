f7aa02150df5ae05888dd4e1b8d6cb4dcbf8102d
robpatterson13 <robertpatterson1313@gmail.com>
2025-02-19 12:59:44 -0500

    need to prove last premise of IHh2

--- specification/language.v
+++ specification/language.v
@@ -218,6 +218,81 @@ Proof.
     + rewrite <- String.eqb_sym. rewrite Heq. apply IH.
 Qed.
 
+Lemma subst_neq_id_commute:
+  forall (x s : string) (v n : nat) (e : tm),
+    x <> s ->
+    (subst s n (subst x v e)) = (subst x v (subst s n e)).
+  intros.
+  induction e; subst.
+  - destruct (String.eqb x s) eqn:H_x_s; destruct (String.eqb s s0) eqn:H_s_s0; subst;
+      try apply String.eqb_eq in H_x_s; try contradiction.
+    + simpl.
+      rewrite H_s_s0.
+      simpl.
+      apply String.eqb_eq in H_s_s0.
+      rewrite H_s_s0 in H.
+      apply String.eqb_neq in H; rewrite H.
+      simpl.
+      apply String.eqb_eq in H_s_s0; rewrite H_s_s0.
+      reflexivity.
+    + simpl.
+      rewrite H_s_s0.
+      simpl.
+      destruct (String.eqb x s0); simpl.
+      * reflexivity.
+      * rewrite H_s_s0; reflexivity.
+  - reflexivity.
+  - simpl; rewrite IHe1; rewrite IHe2; reflexivity.
+  - simpl; rewrite IHe; reflexivity.
+  - simpl; rewrite IHe1.
+    destruct (String.eqb x s0); destruct (String.eqb s s0); subst; try reflexivity.
+    + simpl; rewrite IHe2; reflexivity.
+Qed.
+
+Lemma subst_eq_id_erasure:
+  forall (x s : string) (v n : nat) (e : tm),
+    x = s ->
+    (subst s n (subst x v e)) = (subst x v e).
+  intros.
+  induction e; subst; simpl.
+  - destruct (String.eqb s s0) eqn:H_x0_s0; subst; simpl.
+    + reflexivity.
+    + rewrite H_x0_s0.
+      reflexivity.
+  - reflexivity.
+  - rewrite IHe1; rewrite IHe2; reflexivity.
+  - rewrite IHe; reflexivity.
+  - rewrite IHe1.
+    destruct (String.eqb s s0); simpl.
+    + reflexivity.
+    + rewrite IHe2; reflexivity.
+Qed.
+
+Lemma id_neq_sym:
+  forall (a b : string), b <> a -> a <> b.
+  intros.
+  unfold not.
+  unfold not in H.
+  intro h_eq; rewrite h_eq in H.
+  auto.
+Qed.
+          
+Lemma subst_many_subst_commute:
+  forall (x : string) (v : nat) (g : smap) (e : tm),
+    (subst_many (filter (fun y => negb (String.eqb (fst y) x)) g) (subst x v e))
+    = (subst x v (subst_many (filter (fun y => negb (String.eqb (fst y) x)) g) e)).
+  induction g; simpl.
+  - reflexivity.
+  - destruct a; simpl; destruct (negb (String.eqb s x)) eqn:H_s_x; simpl.
+    + intro e.
+      apply Bool.negb_true_iff in H_s_x. apply String.eqb_neq in H_s_x.
+      apply id_neq_sym in H_s_x.
+      rewrite (subst_neq_id_commute x s v n e H_s_x).
+      rewrite (IHg (subst s n e)).
+      reflexivity.
+    + apply IHg.
+Qed.
+
 (* TODO: write the rest of subst_many lemmas, AFTER looking at the main proof *)
   
 (*
@@ -301,9 +376,54 @@ Theorem noninterference G e t :
     inversion h_eval2; subst.
 
     unfold has_sem_type in IHh2.
-    specialize (IHh2 (update (filter (fun y => negb (String.eqb (fst y) x)) g1) x v0) (update (filter (fun y => negb (String.eqb (fst y) x)) g2) x v3) v1 v2).
-  }
+    specialize (IHh2 (update (filter (fun y => negb (String.eqb (fst y) x)) g1) x v0) (update (filter (fun y => negb (String.eqb (fst y) x)) g2) x v3) v1 v2).  
+            
+    (* want to generalize following assertions as lemmas over
+       arbitrary gammas without x *)        
+    assert (H_pull_out_inner_subst : forall x v s n e g,
+               x <> s ->
+               (subst_many (filter (fun y => negb (String.eqb (fst y) x)) g) (subst s n (subst x v e)))
+               = (subst x v (subst_many (filter (fun y => negb (String.eqb (fst y) x)) g) (subst s n e)))). {
+      intros.
+      induction g; simpl.
+      - apply (subst_neq_id_commute x0 s v n e).
+        exact H.
+      - destruct a; simpl; destruct (String.eqb s0 x0) eqn:H_s0_x0; simpl.
+        + apply IHg.
+        + rewrite (subst_neq_id_commute x0 s v n e).
+          rewrite (subst_neq_id_commute x0 s0 v n0 (subst s n e)).
+          apply (subst_many_subst_commute x0 v g (subst s0 n0 (subst s n e))).
+          apply String.eqb_neq in H_s0_x0.
+          apply id_neq_sym in H_s0_x0; exact H_s0_x0.
+          exact H.
+    }
+    
+    assert (H_update_subst_equiv : forall g v e,
+               (subst_many (update (filter (fun y => negb (String.eqb (fst y) x)) g) x v) e)
+               = (subst x v (subst_many (filter (fun y => negb (String.eqb (fst y) x)) g) e))). {
+      simpl.
+      induction g; simpl.
+      - reflexivity.
+      - destruct a; simpl.
+        destruct (negb (String.eqb s x)) eqn:H_eq; simpl.
+        + apply Bool.negb_true_iff in H_eq; apply String.eqb_neq in H_eq.
+          intros v e; specialize (H_pull_out_inner_subst x v s n e g); auto.
+        + apply IHg.
+    }
+    
+    rewrite (H_update_subst_equiv g1 v0 e2) in IHh2.
+    rewrite (H_update_subst_equiv g2 v3 e2) in IHh2.
+     
+    assert (forall Gamma x t g1 g2 v1 v2,
+               subst_rel (update Gamma x t)
+                 (update g1 x v1)
+                 (update g2 x v2)). {
+      admit.
+    }
+
+    specialize (H Gamma x t1 (filter (fun y => negb (String.eqb (fst y) x)) g1) (filter (fun y => negb (String.eqb (fst y) x)) g2) v0 v3).
+    apply (IHh2 H H4 H6).
+    
 Admitted.
-      
     
     
